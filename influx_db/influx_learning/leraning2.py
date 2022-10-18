e1a0d2afa76b9cb4d768178e8c3aca4240a9f8fd
jueta <jpmm.brasil@hotmail.com>
2022-10-18 15:13:27 +0200

    trying

--- influx_db/influx_learning/leraning2.py
+++
@@ -1,7 +0,0 @@
-import influxdb_client, os, time
-from influxdb_client import InfluxDBClient, Point, WritePrecision 
-from influxdb_client.client.write_api import SYNCHRONOUS
-
-
-
-sample.data(set: "airSensor")
