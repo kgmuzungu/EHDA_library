e7468c35be646527dbdb330145a16b0989078ee8
jueta <jpmm.brasil@hotmail.com>
2022-11-01 16:26:12 +0100

    plotting improvements

--- offline_treatment/joao_progress/data_analysis.py
+++ offline_treatment/joao_progress/data_analysis.py
@@ -26,27 +26,32 @@ with open(file_path, 'r') as data_file:
 
 # MEASUREMENT DATA
 
-print("measurements_data_sample")
+print("\nmeasurements_data_sample\n")
 measurements_data_sample = pd.json_normalize(
     data['measurements'], 
-    record_path =["data [nA]"], 
-    record_prefix='data-',
-    meta=["name", "flow rate [m3/s]", "voltage", "current PS", "temperature", "humidity", "spray mode"]
+    record_path = ["data [nA]"], 
+    record_prefix ='data-',
+    meta = ["name", "flow rate [m3/s]", "voltage", "current PS", "temperature", "humidity", "spray mode"]
 )
-print(measurements_data_sample)
+print(measurements_data_sample.info())
 
-print("measurements_data_window")
+print("\nmeasurements_data_window\n")
 measurements_data_window = pd.json_normalize(data['measurements'])
-print(measurements_data_window)
+print(measurements_data_window.info())
 
 # PROCESSING DATA
 
-# processing_data_sample = pd.json_normalize(data['processing'], "psd welch", ["mean", "variance", "deviation", "median", "rms", "maximum variation distance"])
-# print(processing_data_sample)
+print("\nprocessing_data_sample\n")
+processing_data_sample = pd.json_normalize(
+    data['processing'],
+    record_path = "psd welch",   # what is psd welch??   is it really the record path??
+    meta = ["mean", "variance", "deviation", "median", "rms", "maximum variation distance"]
+)
+print(processing_data_sample.info())
 
-print("processing_data_window")
+print("\nprocessing_data_window\n")
 processing_data_window = pd.json_normalize(data['processing'])
-print(processing_data_window)
+print(processing_data_window.info())
 
 
 
@@ -56,7 +61,7 @@ print(processing_data_window)
 #              PLOTTING
 ######################################
 
-fig, axs = plt.subplots(3, 1, sharex=True)
+fig, axs = plt.subplots(9, 1)
 
 axs[0].set(ylabel='data [nA]')
 axs[0].plot(measurements_data_sample['data-0'])
@@ -66,6 +71,27 @@ axs[1].plot(measurements_data_sample['voltage'])
 
 axs[2].set(ylabel='current PS')
 axs[2].plot(measurements_data_sample['current PS'])
+
+
+
+axs[3].set(ylabel='mean')
+axs[3].scatter( processing_data_window.index, processing_data_window['mean'])
+
+axs[4].set(ylabel='variance')
+axs[4].scatter( processing_data_window.index, processing_data_window['variance'])
+
+axs[5].set(ylabel='deviation')
+axs[5].scatter( processing_data_window.index, processing_data_window['deviation'])
+
+axs[6].set(ylabel='median')
+axs[6].scatter( processing_data_window.index, processing_data_window['median'])
+
+axs[7].set(ylabel='rms')
+axs[7].scatter( processing_data_window.index, processing_data_window['rms'])
+
+axs[8].set(ylabel='maximum variation distance')
+axs[8].scatter( processing_data_window.index, processing_data_window['maximum variation distance'])
+
 
 
 
