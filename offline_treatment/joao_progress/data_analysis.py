236892966e6a88ca62dff0fe1ebf5dfe6534819b
jueta <jpmm.brasil@hotmail.com>
2022-11-02 10:50:45 +0100

    better data organization

--- offline_treatment/joao_progress/data_analysis.py
+++ offline_treatment/joao_progress/data_analysis.py
@@ -10,12 +10,14 @@ from pandas.io.json import json_normalize
 import json
 import matplotlib.pyplot as plt
 
-
 warnings.filterwarnings('ignore')
 
-file_path = "summer2022/rampsetup9ethanol_all shapes_5.000040000000001e-10m3_s.json"
+file_path1 = "summer2022/rampsetup9ethanol_all shapes_5.000040000000001e-10m3_s.json"
+file_path2 = "summer2022/rampsetup9paraffin_all shapes_1.6666799999999999e-09m3_s.json"
+file_path3 = "summer2022/rampsetup9water60alcohol40_all shapes_1.416678e-09m3_s.json"
+file_path4 = "summer2022/rampsetup92propanol_all shapes_1.777792e-09m3_s.json"
 
-with open(file_path, 'r') as data_file:    
+with open(file_path1, 'r') as data_file:    
     data = json.loads(data_file.read())  
 
 
@@ -24,37 +26,22 @@ with open(file_path, 'r') as data_file:
 ######################################
 
 
-# MEASUREMENT DATA
-
-print("\nmeasurements_data_sample\n")
-measurements_data_sample = pd.json_normalize(
+print("\ndata_sample\n")
+data_sample = pd.json_normalize(
     data['measurements'], 
     record_path = ["data [nA]"], 
     record_prefix ='data-',
     meta = ["name", "flow rate [m3/s]", "voltage", "current PS", "temperature", "humidity", "spray mode"]
 )
-print(measurements_data_sample.info())
+print(data_sample.info())
 
-print("\nmeasurements_data_window\n")
+
+print("\data_window\n")
 measurements_data_window = pd.json_normalize(data['measurements'])
-print(measurements_data_window.info())
-
-# PROCESSING DATA
-
-print("\nprocessing_data_sample\n")
-processing_data_sample = pd.json_normalize(
-    data['processing'],
-    record_path = "psd welch",   # what is psd welch??   is it really the record path??
-    meta = ["mean", "variance", "deviation", "median", "rms", "maximum variation distance"]
-)
-print(processing_data_sample.info())
-
-print("\nprocessing_data_window\n")
 processing_data_window = pd.json_normalize(data['processing'])
-print(processing_data_window.info())
-
-
-
+data_window = [measurements_data_window, processing_data_window]
+data_window = pd.concat(data_window, axis=1)
+print(data_window.info())
 
 
 ######################################
@@ -64,36 +51,31 @@ print(processing_data_window.info())
 fig, axs = plt.subplots(9, 1)
 
 axs[0].set(ylabel='data [nA]')
-axs[0].plot(measurements_data_sample['data-0'])
+axs[0].plot(data_sample['data-0'])
 
 axs[1].set(ylabel='voltage')
-axs[1].plot(measurements_data_sample['voltage'])
+axs[1].plot(data_sample['voltage'])
 
 axs[2].set(ylabel='current PS')
-axs[2].plot(measurements_data_sample['current PS'])
-
-
+axs[2].plot(data_sample['current PS'])
 
 axs[3].set(ylabel='mean')
-axs[3].scatter( processing_data_window.index, processing_data_window['mean'])
+axs[3].scatter( data_window.index, data_window['mean'])
 
 axs[4].set(ylabel='variance')
-axs[4].scatter( processing_data_window.index, processing_data_window['variance'])
+axs[4].scatter( data_window.index, data_window['variance'])
 
 axs[5].set(ylabel='deviation')
-axs[5].scatter( processing_data_window.index, processing_data_window['deviation'])
+axs[5].scatter( data_window.index, data_window['deviation'])
 
 axs[6].set(ylabel='median')
-axs[6].scatter( processing_data_window.index, processing_data_window['median'])
+axs[6].scatter( data_window.index, data_window['median'])
 
 axs[7].set(ylabel='rms')
-axs[7].scatter( processing_data_window.index, processing_data_window['rms'])
+axs[7].scatter( data_window.index, data_window['rms'])
 
 axs[8].set(ylabel='maximum variation distance')
-axs[8].scatter( processing_data_window.index, processing_data_window['maximum variation distance'])
-
-
-
+axs[8].scatter( data_window.index, data_window['maximum variation distance'])
 
 
 plt.xlabel('samples')
