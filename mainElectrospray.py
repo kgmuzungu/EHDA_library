ae2c8cebf8120c96d2670ea9df934ffcfd47112b
jueta <jpmm.brasil@hotmail.com>
2022-11-17 10:42:14 +0100

    commenting version

--- mainElectrospray.py
+++ mainElectrospray.py
@@ -17,7 +17,6 @@ from time import gmtime, strftime
 from electrospray import ElectrosprayDataProcessing, ElectrosprayConfig, ElectrosprayMeasurements
 from validation_electrospray import ElectrosprayValidation
 from classification_electrospray import ElectrosprayClassification
-# from aux_functions_electrospray import *
 
 
 from configuration_FUG import *
@@ -35,9 +34,12 @@ import numpy as np
 
 
 
-
-# entry point
+# *************************************
+#            MAIN FUNCTION
+# *************************************
 if __name__ == '__main__':
+
+
 
 
     # *************************************
@@ -142,15 +144,11 @@ if __name__ == '__main__':
 
 
     # **************************************
-    #                FUG
+    #              FUG INIT
     # **************************************
     obj_fug_com = FUG_initialize(fug_COM_port)  # parameter: COM port idx
     print("obj_fug_com: ", obj_fug_com)
-
-
-
-    # fig, ax = plt.subplots(3)
-
+    get_voltage_from_PS(obj_fug_com)
 
 
 
@@ -160,7 +158,10 @@ if __name__ == '__main__':
 
     threads = list()
 
-    get_voltage_from_PS(obj_fug_com)
+
+    # 
+    #           FUG   ->   Power supply controller thread. It will be the future actuator thread.
+    #
 
     if MODERAMP:
         txt_mode = "ramp"
@@ -192,18 +193,21 @@ if __name__ == '__main__':
                                                     voltage_stop))
         step_sequency_thread.start()
 
+    # 
+    #           VIDEO   ->   Camera trigger thread using arduino microcontroller
+    #
 
-    # Video Thread
     makeVideo_thread = threading.Thread(
         target=cameraTrigger.activateTrigger, name='video reccording thread', args=(arduino_COM_port,))
     threads.append(makeVideo_thread)
     makeVideo_thread.start()
 
-
+    # 
+    #          DATA ACQUISITION  ->   Data acquisition + data proccessing + data saving thread  (it will be the future sensor thread)
+    #   
 
     data_queue = queue.Queue(maxsize=10)
 
-    # data_acquisition_thread
     data_acquisition_thread = threading.Thread(
         target=data_acquisition.data_acquisition,
         name='Data acquisition thread',
@@ -229,217 +233,28 @@ if __name__ == '__main__':
     data_acquisition_thread.start()
 
 
-    # # plotting_thread
-    # plotting_thread = threading.Thread(
-    #     target=plotting.real_time_plot,
-    #     name='Real Time Plotting thread',
-    #     args=(
-    #         data_queue, 
-    #         event,
-    #     )
-    # )
-    # threads.append(plotting_thread)
-    # plotting_thread.start()
+    # **************************************
+    #              PLOTTING
+    # **************************************
 
-
-    
+    #  plotting is not a thread. It is a function running in a loop in the main.
     fig, ax, ln0, ln1, ln2, bg = plotting.start_plot(data_queue, event)
-# # **************************************
-# #              MAIN LOOP
-# # **************************************
-
     while not event.is_set() or not data_queue.empty():
         plotting.real_time_plot(data_queue, event, fig, ax, ln0, ln1, ln2, bg)
 
-    # fazer funcao de saida do loop
 
 
 
+    # # **************************************
+    # #                EXIT
+    # # **************************************
 
-
-
-# # **************************************
-# #              PLOTTING
-# # **************************************
-# try:
-
-#     # check here to plot the transfer function of the filter
-#     # https://stackoverflow.com/questions/25191620/creating-lowpass-filter-in-scipy-understanding-methods-and-units
-#     # ax[0].text(.5, .5, 'blabla', animated=True)
-
-#     # animated=True tells matplotlib to only draw the artist when we explicitly request it
-#     (ln0,) = ax[0].plot(np.arange(0, len(datapoints) * time_step, time_step), datapoints, animated=True)
-
-#     (ln1,) = ax[1].plot(np.arange(0, len(datapoints_filtered) * time_step, time_step), datapoints_filtered,
-#                         animated=True)
-#     freq = np.fft.fftfreq(len(datapoints_filtered), d=time_step)
-
-#     (ln2,) = ax[2].plot(freq[0:500], np.zeros(500), animated=True)
-
-#     ax[0].set(xlabel='time [s]', ylabel='current (nA)', title='osci reading', ylim=[-3e2, 3e2])
-#     ax[1].set(xlabel='time', ylabel='nA', title='LP filtered', ylim=[-1e1, 5e2])
-#     ax[2].set(xlabel='Frequency [Hz]', ylabel='mag', title='fourier transform', ylim=[0, 1e6])
-#     # freqs_psd, psd = signal.welch(datapoints)
-#     # (ln3,) = ax[3].semilogx(freqs_psd, psd)
-#     # ax[3].set(xlabel='Frequency [Hz]', ylabel='Power', title='power spectral density')
-#     figManager = plt.get_current_fig_manager()
-#     figManager.window.showMaximized()
-
-#     # make sure the window is raised, but the script keeps going
-#     plt.show(block=False)
-#     plt.pause(0.1)
-
-#     # get copy of entire figure (everything inside fig.bbox) sans animated artist
-#     bg = fig.canvas.copy_from_bbox(fig.bbox)
-#     # draw the animated artist, this uses a cached renderer
-#     ax[0].draw_artist(ln0)
-#     ax[1].draw_artist(ln1)
-#     ax[2].draw_artist(ln2)
-
-#     fig.canvas.blit(fig.bbox)
-
-# except:
-#     print("Failed make iterable plot")
-#     sys.exit(1)
-
-
-# # **************************************
-# #              MAIN LOOP
-# # **************************************
-
-# try:
-
-#     # get_voltage_from_PS(obj_fug_com)
-
-#     for j in range(number_measurements):
-
-#         # reset the background back in the canvas state, screen unchange
-#         fig.canvas.restore_region(bg)
-
-#         voltage_from_PS = 1# get_voltage_from_PS(obj_fug_com)
-#         current_from_PS = 1 #get_current_from_PS(obj_fug_com)
-#         current_array.append(current_from_PS)
-#         voltage_array.append(voltage_from_PS)
-#         print("Actual voltage: " + str(voltage_from_PS) + " for the measurement " + str(
-#             j) + " actual current: " + str(current_from_PS))
-
-
-#         #-----------Processing stuff----------
-#         electrospray_data = ElectrosprayMeasurements(liquid, datapoints, voltage_from_PS, Q, temperature,
-#                                                         humidity, day_measurement, current_shape, current_from_PS)
-
-#         electrospray_processing.calculate_filter(a, b, electrospray_data.data)
-#         electrospray_processing.calculate_fft_raw(electrospray_data.data)
-
-#         electrospray_processing.calculate_statistics(electrospray_data.data,
-#                                                         electrospray_processing.datapoints_filtered)
-#         electrospray_processing_freq, electrospray_processing_psd = electrospray_processing.calculate_power_spectral_density(
-#             electrospray_data.data)
-
-
-#         max_data, quantity_max_data, percentage_max = electrospray_processing.calculate_peaks_signal(
-#             electrospray_data.data)
-
-#         max_fft_peaks, cont_max_fft_peaks = electrospray_processing.calculate_peaks_fft(electrospray_data.data)
-
-#         classification_sjaak = electrospray_classification.do_sjaak(electrospray_processing.mean_value,
-#                                                             electrospray_processing.med,
-#                                                             electrospray_processing.stddev,
-#                                                             electrospray_processing.psd_welch,
-#                                                             electrospray_processing.variance)
-
-#         classification_monica = electrospray_classification.do_monica(float(max_data), float(quantity_max_data),
-#                                                                         float(percentage_max), float(Q), max_fft_peaks,
-#                                                                         cont_max_fft_peaks)
-#         txt_sjaak_str = str(classification_sjaak)
-#         txt_monica_str = str(classification_monica)
-
-#         current_shape = {
-#             "Sjaak":  str(classification_sjaak),
-#             "Monica": str(classification_monica),
-#         }
-#         electrospray_data.set_shape(current_shape)
-
-#         txt_max_peaks = " Max: " + str(max_data) + " Quantity max: " + str(
-#             quantity_max_data) + " Percentage: " + str(percentage_max)
-
-#         electrospray_processing.calculate_fft_filtered()
-#         electrospray_processing.calculate_fft_peaks()
-
-#         #-----------<\Processing stuff----------
-
-#         #-----------Plotting stuff----------
-
-#         ln0.set_ydata(electrospray_data.data)
-#         ln1.set_ydata(electrospray_processing.datapoints_filtered)
-#         ln2.set_ydata(
-#             (electrospray_processing.fourier_transform[0:500]))
-#         ax[0].legend(bbox_to_anchor=(1.05, 1),
-#                         loc='upper left', borderaxespad=0.)
-
-#         # re-render the artist, updating the canvas state, but not the screen
-#         ax[0].draw_artist(ln0)
-#         ax[1].draw_artist(ln1)
-#         ax[2].draw_artist(ln2)
-
-#         fig.canvas.manager.set_window_title('Sjaak: ' + txt_sjaak_str + 'monnica' + txt_monica_str + '; Peaks:' + txt_max_peaks +
-#                                             " voltage_PS= " + str(voltage_from_PS) + " current_PS= " + str(
-#             current_from_PS * 1e6) + " current mean osci= " + str(electrospray_processing.mean_value))
-
-
-#         """df = pd.DataFrame({str(j): ['Sjaak: ' + txt_sjaak_str + ' ; Peaks:' + txt_max_peaks +
-#                                             " voltage_PS= " + str(voltage_from_PS) + " current_PS= " + str(
-#             current_from_PS * 1e6) + " current mean osci= " + str(electrospray_processing.mean_value) ] } ) """
-
-
-#         # copy the image to the GUI state, but screen might not be changed yet
-#         fig.canvas.blit(fig.bbox)
-#         # flush any pending GUI events, re-painting the screen if needed
-#         fig.canvas.flush_events()
-
-#         #-----------<\Plotting stuff----------
-
-
-#         if current_shape["Sjaak"] == "cone jet" and FLAG_PLOT:
-#             electrospray_validation.set_data_from_dict_liquid(
-#                 electrospray_config_liquid_setup_obj.get_json_liquid())
-#             electrospray_validation.calculate_scaling_laws_cone_jet(electrospray_data.data,
-#                                                                     electrospray_processing.mean_value, Q)
-#         if append_array_data:
-#             d_electrospray_measurements = electrospray_data.get_measurements_dictionary()
-#             a_electrospray_measurements.append(d_electrospray_measurements)
-
-#         if append_array_processing:
-#             d_electrospray_processing = electrospray_processing.get_statistics_dictionary()
-#             a_electrospray_processing.append(d_electrospray_processing)
-
-#         # Start new measurement:
-#         scp.start()
-#         # Wait for measurement to complete:
-#         while not scp.is_data_ready:
-#             time.sleep(0.05)  # 50 ms delay, to save CPU time
-#         # Get more data:
-#         data = scp.get_data()
-#         datapoints = np.array(data[0]) * multiplier_for_nA
-
-# except Exception as e:
-#     print('Exception: ' + e.message)
-#     print('Main loop failed')
-#     sys.exit(1)
-#     # Close oscilloscope:
-#     del scp
-
-
-# # wait until threads finish
-# if MODERAMP:
-#     ramp_sequency_thread.join()
-#     makeVideo_thread.join()
-#     FUG_sendcommands(obj_fug_com, ['U 0'])
-# else:
-#     step_sequency_thread.join()
-#     makeVideo_thread.join()
-#     FUG_sendcommands(obj_fug_com, ['U 0'])
-
-
-# logging.info('Finished')
-# sys.exit(0)
+        # fazer funcao de saida do loop
+        #     ramp_sequency_thread.join()
+        #     makeVideo_thread.join()
+        #     FUG_sendcommands(obj_fug_com, ['U 0'])
+        #     sys.exit(1)
+        #     # Close oscilloscope:
+        #     del scp
+        # logging.info('Finished')
+        # sys.exit(0)
