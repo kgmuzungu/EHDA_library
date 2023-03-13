4a86787eef23ecbdeea1749f736d21d9a6e6521e
jueta <jpmm2209@gmail.com>
2023-03-13 09:43:13 +0100

    documentation

--- data_analysis/plot_raw_data.py
+++ data_analysis/plot_raw_data.py
@@ -1,35 +1,34 @@
 
 import pandas as pd
-import json
 import matplotlib.pyplot as plt
 import numpy as np
 
+df = pd.read_feather('Data/map_23-03-02.feather')
 
 
-data = pd.read_feather('../classified_data/data1.feather')
+df = df.loc[df['flow_rate'] == 0.7]
 
-
-
-colors = {'Cone Jet':'red', 'Dripping':'green', 'Intermittent':'blue', 'Multi Jet':'purple', 'Undefined':'black', 'Corona':'cyan'}
-data.info()
-
+df.reset_index(drop=True).head()
 
 size = 50000
 last_value = 0
 
-for sample in data.index:
-    if data['class'][sample] == 'Dripping':
-        plt.plot(np.arange(last_value, last_value + size), data['data'][sample], c='green', label='Dripping')
-    elif data['class'][sample] == 'Intermittent':
-        plt.plot(np.arange(last_value, last_value + size), data['data'][sample], c='blue', label='Intermittent')
-    elif data['class'][sample] == 'Cone Jet':
-        plt.plot(np.arange(last_value, last_value + size), data['data'][sample], c='red', label='Cone Jet')
-    elif data['class'][sample] == 'Multi Jet':
-        plt.plot(np.arange(last_value, last_value + size), data['data'][sample], c='purple', label='Multi Jet')
-    elif data['class'][sample] == 'Undefined':
-        plt.plot(np.arange(last_value, last_value + size), data['data'][sample], c='black', label='Undefined')
-    elif data['class'][sample] == 'Corona':
-        plt.plot(np.arange(last_value, last_value + size), data['data'][sample], c='cyan', label='Corona')
+for sample in df.index:
+    if df['spray_mode'][sample] == 'Dripping':
+        plt.plot(np.arange(last_value, last_value + size)/1e5, df['current'][sample], c='green', label='Dripping')
+    elif df['spray_mode'][sample] == 'Intermittent':
+        plt.plot(np.arange(last_value, last_value + size)/1e5, df['current'][sample], c='blue', label='Intermittent')
+    elif df['spray_mode'][sample] == 'Cone Jet':
+        plt.plot(np.arange(last_value, last_value + size)/1e5, df['current'][sample], c='red', label='Cone Jet')
+    elif df['spray_mode'][sample] == 'Multi Jet':
+        plt.plot(np.arange(last_value, last_value + size)/1e5, df['current'][sample], c='purple', label='Multi Jet')
+    elif df['spray_mode'][sample] == 'Undefined':
+        plt.plot(np.arange(last_value, last_value + size)/1e5, df['current'][sample], c='black', label='Undefined')
+    elif df['spray_mode'][sample] == 'Corona':
+        plt.plot(np.arange(last_value, last_value + size)/1e5, df['current'][sample], c='cyan', label='Corona')
     last_value += size
+
+plt.ylim(-50, 250)
+plt.xlim(right=113)
 
 plt.show()
